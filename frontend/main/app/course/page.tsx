c3c73f001d0e0706f7df11dd221f918f0ca90234
Phu (Jack) Nguyen <phu.h.g.nguyen@gmail.com>
2023-11-30 03:27:09 -0600

    frontend: update recommender UI

--- frontend/main/app/course/page.tsx
+++ frontend/main/app/course/page.tsx
@@ -3,7 +3,6 @@
 import { useState } from "react";
 
 export default function CoursesPage() {
-  const [result, setResult] = useState("");
   const courses = [
     "engr102",
     "csce121",
@@ -15,7 +14,7 @@ export default function CoursesPage() {
     "csce314",
     "csce315",
   ];
-  const initialState = {
+  const initialRatings = {
     engr102: 1,
     csce121: 1,
     csce181: 1,
@@ -27,10 +26,15 @@ export default function CoursesPage() {
     csce315: 1,
   };
 
-  const [courseState, setCourseState] = useState(initialState);
+  const initialResults = {
+    csce470: 5,
+  };
+
+  const [courseRatings, setCourseRatings] = useState(initialRatings);
+  const [result, setResult] = useState(initialResults);
 
   const handleSliderChange = (event, value) => {
-    setCourseState((prevState) => ({
+    setCourseRatings((prevState) => ({
       ...prevState,
       [value]: parseFloat(event.target.value),
     }));
@@ -39,7 +43,7 @@ export default function CoursesPage() {
   const handleSubmit = async () => {
     fetch("https://jsonplaceholder.typicode.com/posts", {
       method: "POST",
-      body: JSON.stringify(courseState),
+      body: JSON.stringify(courseRatings),
       headers: {
         "Content-type": "application/json; charset=UTF-8",
       },
@@ -47,7 +51,7 @@ export default function CoursesPage() {
       .then((response) => response.json())
       .then((json) => {
         console.log(json);
-        setResult("Check the Console and Network tabs for results yo.");
+        setResult({ "Expecting response from backend": 0 });
       });
   };
 
@@ -62,17 +66,18 @@ export default function CoursesPage() {
       {courses.map((course) => (
         <div key={course}>
           <h3 className="text-3xl md:text-2xl">{course}</h3>
-          <div>
+          <div className="flex items-center">
             <input
               type="range"
-              value={courseState[course]}
+              className="w-[85%]"
+              value={courseRatings[course]}
               min={1}
               max={5}
               step={1}
               onChange={(e) => handleSliderChange(e, course)}
             />
-            <div className="text-center font-bold mt-2" id="slider-output">
-              {courseState[course]}
+            <div className="text-center text-2xl md:text-2x1 font-bold mt-2 ml-10">
+              {courseRatings[course]}
             </div>
           </div>
         </div>
@@ -85,27 +90,36 @@ export default function CoursesPage() {
       {InfoCard}
       <div className="flex flex-row gap-4">
         <div className="p-4 md:p-8 rounded bg-[#17141c] w-full max-h-[85%] overflow-hidden">
-          <h2 className="text-2xl md:text-2xl">🤔 Input Information</h2>
+          <h2 className="text-2xl md:text-2xl">
+            🤔 Input Your Overall Course Ratings
+          </h2>
+          <p>How to calculate this score?</p>
+          <p>
+            (Difficulty + Happiness + Willingness to Take Again + Grade) / 4
+            rounded to the closet number
+          </p>
           {CourseCard}
         </div>
-        <div className="p-4 md:p-8 rounded bg-[#17141c] w-full max-h-[85%] overflow-hidden">
+        <div className="p-4 md:p-8 rounded bg-[#17141c] w-full max-h-[85%] overflow-hidden flex flex-col gap-4">
           <h2 className="text-2xl md:text-2xl">💡 Recommended Course</h2>
-          {courses.map((course) => (
-            <div key={course}>
-              <p>
-                {course}: {courseState[course]}
-              </p>
-            </div>
-          ))}
-          <button
-            type="submit"
-            className="shrink-0 px-8 py-4 bg-[#fb442c] rounded w-28"
-            onClick={handleSubmit}
-          >
-            Submit
-          </button>
-          <br></br>
-          Result: {result}
+          <div className="flex justify-center">
+            <button
+              type="submit"
+              className="shrink-0 px-8 py-4 bg-[#fb442c] rounded"
+              onClick={handleSubmit}
+            >
+              <span className="w-full text-center">Get Recommendations</span>
+            </button>
+          </div>
+          <div>
+            {Object.keys(result).map((course) => (
+              <div key={course}>
+                <p className="text-center text-2xl md:text-2xl font-bold mt-2 ml-10">
+                  {course}: {result[course]}
+                </p>
+              </div>
+            ))}
+          </div>
         </div>
       </div>
     </div>
